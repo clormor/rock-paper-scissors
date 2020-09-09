921b73793ee026b64ab177028a55e1fab99f56c9
Chris Lormor <clormor@palantir.com>
2020-09-09 15:04:08 +0100

    refactor comments

--- scripts/rock-paper-scissor.js
+++ scripts/rock-paper-scissor.js
@@ -15,35 +15,35 @@ const promptClass = 'console-prompt'
 const possibleChoices = ['rock', 'paper', 'scissors']
 
 /**
- * Compare two choices in a game rock paper scissors
- * @param {*} choice1 The player's choice
- * @param {*} choice2 The computer's choice
- * @returns >0 if choice1 is the winner, <0 if choice2 is the winner, 0 if it's a tie
+ * Determine the winner in a game rock paper scissors
+ * @param {number} playerChoice The player's choice
+ * @param {number} computerChoice The computer's choice
+ * @returns >0 if the player wins, <0 if the computer wins, 0 for a tie
  */
-let compareChoices = (choice1, choice2) => {
-    choice1 = parseChoice(choice1)
-    choice2 = parseChoice(choice2)
+let compareChoices = (playerChoice, computerChoice) => {
+    playerChoice = parseChoice(playerChoice)
+    computerChoice = parseChoice(computerChoice)
 
-    if (choice1 === choice2) {
+    if (playerChoice === computerChoice) {
         return 0
     }
 
-    if (choice1 === 'rock') {
-        return (choice2 === 'scissors') ? 1 : -1
+    if (playerChoice === 'rock') {
+        return (computerChoice === 'scissors') ? 1 : -1
     }
 
-    if (choice1 === 'paper') {
-        return (choice2 === 'rock') ? 1 : -1
+    if (playerChoice === 'paper') {
+        return (computerChoice === 'rock') ? 1 : -1
     }
 
-    if (choice1 === 'scissors') {
-        return (choice2 === 'paper') ? 1 : -1
+    if (playerChoice === 'scissors') {
+        return (computerChoice === 'paper') ? 1 : -1
     }
 }
 
 /**
  * Parse a choice to ensure that it is valid (e.g. 'rock', 'paper', 'scissors' or an acceptable variant)
- * @param {Pa} choice The choice to parse
+ * @param {string} choice The choice to parse
  * @returns The parsed choice (guaranteed to be strictly equal to 'rock', 'paper' or 'scissors') or undefined
  */
 function parseChoice(choice) {
@@ -78,7 +78,7 @@ function getConsole() {
 
 /**
  * Write a message to the console
- * @param {*} message The message to display on the console
+ * @param {string} message The message to display on the console
  */
 function writeToConsole(message) {
     let para = document.createElement('p')
@@ -94,30 +94,30 @@ function getCurrentPrompt() {
 }
 
 /**
- * Prompt the user with the next appropriate action. This could be to;
- * # Make their turn in a game, if not all rounds have been completed
- * # Decide whether or not to play a new game or quit
+ * Prompt the user to perform the next appropriate action
+ * @param {string} message An optional message to display before the prompt
  */
-function promptPlayer() {
+function promptNextAction(message) {
+    writeToConsole(message)
     if (roundsPlayed >= roundsPerGame) {
         writeOverallResult()
         promptPlayAgain()
     } else {
-        playerTurn()
+        promptPlayerTurn()
     }
 }
 
 /**
  * Prompt the player to take their turn in a game of rock paper scissors
  */
-function playerTurn() {
+function promptPlayerTurn() {
     let message = 'Round ' + (roundsPlayed + 1) + '. Enter choice [rock/paper/scissors]'
     promptUser(message, parsePlayerChoiceOnEnter)
 }
 
 /**
  * Add a new user prompt to the console
- * @param {*} message The message to display before the input
+ * @param {string} message The message to display before the input
  * @param  {...any} onKeyUp The function(s) to execute on 'keyup' events on the input
  */
 function promptUser(message, ...onKeyUp) {
@@ -139,8 +139,8 @@ function promptUser(message, ...onKeyUp) {
 
 /**
  * Perform an action only after the user presses Enter
- * @param {*} event The event that fired
- * @param {*} onEnter The function to execute if the event was triggered by the Enter key
+ * @param {event} event The event that fired
+ * @param {Function} onEnter The function to execute if the event was triggered by the Enter key
  */
 function performOnEnter(event, onEnter) {
     if (event.key === 'Enter') {
@@ -150,7 +150,7 @@ function performOnEnter(event, onEnter) {
 
 /**
  * Function callback to parse the player's choice when they press Enter
- * @param {*} event The event that fired
+ * @param {event} event The event that fired
  */
 function parsePlayerChoiceOnEnter(event) {
     performOnEnter(event, parsePlayerChoice)
@@ -158,7 +158,7 @@ function parsePlayerChoiceOnEnter(event) {
 
 /**
  * Function callback to decide whether or not to restart the game when the user presses Enter
- * @param {*} event The event that fired
+ * @param {event} event The event that fired
  */
 function parseRestartOnEnter(event) {
     performOnEnter(event, parseRestart)
@@ -171,9 +171,9 @@ let parseRestart = () => parseYesOrNo(startGame, endGame, promptPlayAgain)
 
 /**
  * Process a yes / no response and decide how to proceed
- * @param {*} onYes Executed if the response is parsed as 'yes'
- * @param {*} onNo Executed if the response is parsed as 'no'
- * @param {*} onUnrecognised Executed if the response cannot be parsed
+ * @param {Function} onYes Executed if the response is parsed as 'yes'
+ * @param {Function} onNo Executed if the response is parsed as 'no'
+ * @param {Function} onUnrecognised Executed if the response cannot be parsed
  */
 function parseYesOrNo(onYes, onNo, onUnrecognised) {
     let playerChoice = readCurrentPrompt()
@@ -249,13 +249,14 @@ function fixCaretPosition() {
 
 /**
  * Get the contents of the current input prompt
+ * @returns The text contained in the active input prompt
  */
 function readCurrentPrompt() {
     let currentPrompt = getCurrentPrompt()
     // disable the input from future edits
     currentPrompt.setAttribute('disabled', 'disabled')
-    // return its contents (trimmed and lowercased for convenience)
-    return currentPrompt.value.trim().toLowerCase()
+    // return its text content
+    return currentPrompt.value
 }
 
 /**
@@ -265,12 +266,11 @@ function parsePlayerChoice() {
     let playerChoice = readCurrentPrompt()
     playerChoice = parseChoice(playerChoice)
     if (playerChoice) {
-        // if the choice was vald, play out the round
+        // if the choice was valid, play out the round
         playRound(playerChoice)
     } else {
         // if the choice was invalid, alert the user and re-prompt
-        writeToConsole('Unrecgonised Input.')
-        promptPlayer()
+        promptNextAction('Unrecgonised Input.')
     }
 }
 
@@ -283,12 +283,12 @@ function playRound(playerChoice) {
     updateScores(result)
     writeResultOfRound(result, computerChoice)
     roundsPlayed++
-    promptPlayer()
+    promptNextAction()
 }
 
 /**
  * Update the global score variables based on a round result
- * @param {*} result The result of a round
+ * @param {number} result The result of a round
  */
 function updateScores(result) {
     if (result > 0) {
@@ -313,7 +313,7 @@ function writeResultOfRound(result, computerChoice) {
 /**
  * Construct a string to congratulate or console a user based on a given result
  * Assumes the user has won if the result is >0, and lost if the result is <0.
- * @param {*} result The result of either a round or an overall result of several rounds
+ * @param {number} result The result of either a round or an overall result of several rounds
  * @returns An appropriate message in the form of a string
  */
 function generateResultMessage(result) {
@@ -347,11 +347,10 @@ function generateOverallResultMessage() {
  * Starts a new game of rock paper scissors
  */
 function startGame() {
-    writeToConsole('Let\'s play a game of rock paper scissors! Best of ' + roundsPerGame + '...')
     playerScore = 0
     computerScore = 0
     roundsPlayed = 0
-    promptPlayer()
+    promptNextAction('Let\'s play a game of rock paper scissors! Best of ' + roundsPerGame + '...')
 }
 
 /**
